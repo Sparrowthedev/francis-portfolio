e1a3fb123fc9209ad9bbace29c37f9cdd37f30bc
sparrowthedev <fridayogwuche61@gmail.com>
2022-04-22 20:20:23 +0100

    Changes
    created a data.js files in src

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const portfolioItems = [
+    {
+        img: "images/portfolio/portfolio-1.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 1",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-2.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 2",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-3.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 3",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-4.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 4",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-5.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 5",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-6.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 6",
+        desc: "Project name goes here"
+    },
+    {
+        img: "/images/portfolio/portfolio-7.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 7",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-8.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 8",
+        desc: "Project name goes here"
+    },
+    {
+        img: "images/portfolio/portfolio-9.jpg",
+        alt: "portfolio pic",
+        Title: "project name goes here 9",
+        desc: "Project name goes here"
+    }
+]
--- src/react components/Portfolio.jsx
+++ src/react components/Portfolio.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { portfolioItems } from '../data';
 
 const Portfolio = () => {
   return (
@@ -31,107 +32,21 @@ const Portfolio = () => {
 
         {/* <!-- Start the Porfolio content --> */}
         <div class="row">
-            <div class="col-lg-4 col-md-6">
+            {portfolioItems.map(item=>(
+                <div class="col-lg-4 col-md-6">
                 <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-1.jpg" alt="portfolio-1 pic" class="img-fluid"/>
+                    <img src={item.img} alt={item.alt} class="img-fluid"/>
                     <div class="portfolio-info">
                         <div class="caption">
-                            <h4>project name goes here 1</h4>
-                            <p>category project</p>
+                            <h4>{item.Title}</h4>
+                            <p>{item.desc}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-2.jpg" alt="portfolio-2 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 2</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-3.jpg" alt="portfolio-3 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 3</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-4.jpg" alt="portfolio-4 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 4</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-5.jpg" alt="portfolio-5 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 5</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-6.jpg" alt="portfolio-6 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 6</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-7.jpg" alt="portfolio-7 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 7</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-8.jpg" alt="portfolio-8 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 8</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-12">
-                <div class="portfolio-box shadow">
-                    <img src="images/portfolio/portfolio-9.jpg" alt="portfolio-9 pic" class="img-fluid"/>
-                    <div class="portfolio-info">
-                        <div class="caption">
-                            <h4>project name goes here 9</h4>
-                            <p>category project</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
-    </div>
+    </div>    
 </section>
 
   )
